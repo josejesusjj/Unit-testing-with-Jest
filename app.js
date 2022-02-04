a1b4d73a55cd8b4a4ea0b342ee4770f4533e9941
Jose <josejesuscedeno@gmail.com>
2022-02-04 16:59:43 +0000

    falta hacer los tests de fromDollarToYen y fromYenToPound

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+// we declare the function with the exact name "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar){
+    // convert the given valueInDollar to euros
+    let valueInEuro = valueInDollar/1.2
+    let valueInYen = valueInEuro*127.9
+    //return the yen value
+    return valueInYen;
+}
+// we declare the function with the exact name "fromYenToPound"
+const fromYenToPound = function(valueInYen){
+    // convert the given valueInYen to Pounds
+    let valueInEuro = valueInYen/127.9
+    let valueInPound = valueInEuro*0.8  
+    //return the dollar value
+    return valueInPound;
+}
+
 // this is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
@@ -8,4 +32,4 @@ console.log(sum(7,3))
 
 // export the function to be used on other files 
 // (similar to the keyword `export` when using webpack)
-module.exports = { sum };
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
