1c7b67af0c59b4321113133582c37e0a3e673af0
Jose <josejesuscedeno@gmail.com>
2022-02-04 19:08:33 +0000

    testing

--- test.js
+++ test.js
@@ -23,3 +23,16 @@ test("One euro should be 1.206 dollars", function(){
     // this is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
+test("1.2 dollar should be 127.9 yen", function(){
+    //import the function from app.js
+    const { fromDollarToYen } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const yen = fromDollarToYen(1)
+
+    // if 1 dollars are 106.58 yen, then 3.5 dollars should be (106.58*3.5)
+    const expected = 106.58 * 3.5; 
+    
+    // this is the comparison for the unit test
+     expect(fromDollarToYen(3.5)).toBe(373.042); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
